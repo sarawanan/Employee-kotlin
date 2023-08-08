7ff9d78fa28e1a25ee57071e0887421bea8495b0
sarawanan <sarawananm@icloud.com>
2023-08-08 09:42:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/empkotlin/EmpKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.empkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmpKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmpKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/empkotlin/EmpKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.empkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmpKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
